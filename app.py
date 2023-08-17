72ea2727940020995e3b035350561f67a6fea7f9
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-17 14:28:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,401 @@ import streamlit as st
 
 openai.api_key = "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"
 
-st.set_page_config(page_title="Classifier Playground", page_icon="", layout="wide")
+st.set_page_config(page_title="Image Generation Playground", page_icon="", layout="wide")
+
+styles2 = """
+<style>
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    .viewerBadge_link__1S137 {display: none !important;}
+    .col2 {
+        margin: 0em;
+        display: flex;
+        align-items: center;
+        vertical-align: middle;
+        padding-right: 0.875em;
+        margin-top: -0.5em;
+        margin-bottom: 0em;
+    }
+    .left2 {
+        text-align: center;
+        width: 80%;
+        padding-top: 0em;
+        padding-bottom: 0em;
+    }
+    .right2 {
+        text-align: center;
+        width: 20%;
+        padding-top: 0em;
+        padding-bottom: 0em;
+    }
+
+    /* Tooltip container */
+    .tooltip2 {
+        position: relative;
+        margin-bottom: 0em;
+        display: inline-block;
+        margin-top: 0em;
+    }
+
+    /* Tooltip text */
+    .tooltip2 .tooltiptext2 {
+        visibility: hidden;
+        width: 70em;
+        background-color: #03A9F4;
+        color: #FAFAFA;
+        text-align: justify;
+        font-family: sans-serif;
+        display: block; 
+        border-radius: 0.375em;
+        white-space: normal;
+        padding-left: 0.75em;
+        padding-right: 0.75em;
+        padding-top: 0.5em;
+        padding-bottom: 0em;
+        border: 0.1875em solid #FAFAFA;
+
+        /* Position the tooltip text */
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        transform: translateX(-95%);
+
+        /* Fade in tooltip */
+        opacity: 0;
+        transition: opacity 0.5s;
+    }
+
+    /* Tooltip arrow */
+    .tooltip2 .tooltiptext2::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 95.6%;
+        border-width: 0.625em;
+        border-style: solid;
+        border-color: #FAFAFA transparent transparent transparent;
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip2:hover .tooltiptext2 {
+        visibility: visible;
+        opacity: 1;
+    }
+    /* Change icon color on hover */
+    .tooltip2:hover i {
+        color: #FAFAFA;
+    }   
+    /* Set initial icon color */
+    .tooltip2 i {
+        color: #03A9F4;
+    }
+    ul.responsive-ul2 {
+        font-size: 0.8em;
+    }
+    ul.responsive-ul2 li {
+        font-size: 1em;
+    }
+
+    /* Responsive styles */
+    @media (max-width: 1024px) {
+       .col2 {
+            padding-right: 1em;
+            margin-top: 0em;
+        }
+        p.subtext_manual2 {
+            font-size: 3.6em;
+        }
+    .tooltip2 .tooltiptext2 {
+        border-width: 0.6em;
+        border-radius: 1.6em;
+        width: 80em;
+        left: 50%;
+    }
+    .tooltip2 .tooltiptext2::after {
+        border-width: 2em;
+        left: 93.5%;
+    }
+    .tooltip2 {
+        
+    }
+    .tooltip2 i {
+        font-size: 8em;
+        margin-bottom: 0.2em;
+    }
+    ul.responsive-ul2 {
+        font-size: 3.2em;
+    }
+    ul.responsive-ul2 li {
+        font-size: 1em;
+    }
+    }
+</style>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+"""
+
+st.markdown(styles2, unsafe_allow_html=True)
+
+st.markdown("""
+  <style>
+    div.block-container.css-ysnqb2.e1g8pov64 {
+        margin-top: -3em;
+    }
+    div[data-modal-container='true'][key='Modal1'] > div:first-child > div:first-child {
+        background-color: rgb(203, 175, 175) !important;
+    }
+    div[data-modal-container='true'][key='Modal1'] > div > div:nth-child(2) > div {
+        max-width: 3em !important;
+    }
+    div[data-modal-container='true'][key='Modal2'] > div:first-child > div:first-child {
+        background-color: rgb(203, 175, 175) !important;
+    }
+    div[data-modal-container='true'][key='Modal2'] > div > div:nth-child(2) > div {
+        max-width: 3em !important;
+    }
+    .css-gesnqs {
+        background-color: #FCBC24 !important;
+    }
+    .css-fpzaie {
+        background-color: #FCBC24 !important;
+    }
+    .css-5qhjmn {
+        z-index: 1000 !important;
+    }
+    .css-15d9ls5{
+        z-index: 1000 !important;
+    }
+    .css-g6xpsg {
+        z-index: 1000 !important;
+    }
+    .css-2542xv {
+        z-index: 1000 !important;
+    }
+    .css-1h5vz9d {
+        z-index: 1000 !important;
+    }
+    .css-1s3wgy2 {
+        z-index: 1000 !important;
+    }
+    .css-1s3wgy2 {
+        z-index: 1000 !important;
+    }
+    .css-1s3wgy2 {
+        z-index: 1000 !important;
+    }
+    .css-1vb7lhv {
+        z-index: 1000 !important;
+    }
+    .css-mx6j8v {
+        z-index: 1000 !important;
+    }
+    .css-1s3wgy2 {
+        z-index: 1000 !important;
+    }
+            div.css-1inwz65.ew7r33m0 {
+            font-size: 0.8em !important;
+            font-family: sans-serif !important;
+        }
+        div.StyledThumbValue.css-12gsf70.ew7r33m2{
+            font-size: 0.8em !important;
+            font-family: sans-serif !important;
+            color: #FAFAFA !important;
+        }
+        @media (max-width: 1024px) {
+          div.css-1inwz65.ew7r33m0 {
+            font-size: 0.8em !important;
+            font-family: sans-serif !important;
+          }
+          div.StyledThumbValue.css-12gsf70.ew7r33m2{
+            font-size: 0.8em !important;
+            font-family: sans-serif !important;
+            color: #FAFAFA !important;
+        }
+      }
+    @media (max-width: 1024px) {
+        div.block-container.css-ysnqb2.e1g8pov64 {
+            margin-top: -15em !important;;
+        }
+    }
+    div.stButton {
+        display: flex !important;
+        justify-content: center !important;
+    }
+    
+     div.stButton > button:first-child {
+        background-color: #002147;
+        color: #FAFAFA;
+        border-color: #FAFAFA;
+        border-width: 0.15em;
+        width: 100%;
+        height: 0.2em !important;
+        margin-top: 0em;
+        font-family: sans-serif;
+    }
+    div.stButton > button:hover {
+        background-color: #76787A;
+        color: #FAFAFA;
+        border-color: #002147;
+    }
+    @media (max-width: 1024px) {
+    div.stButton > button:first-child {
+        width: 100% !important;
+        height: 0.8em !important;
+        margin-top: 0em;
+        border-width: 0.15em; !important;
+        }
+    }
+    /* The input itself */
+  div[data-baseweb="select"] > div,
+  input[type=number] {
+  color: #FAFAFA;
+  background-color: #4F5254;
+  border: 0.25em solid #002147;
+  font-size: 0.8em;
+  font-family: sans-serif;
+  height: 3em;
+  }
+  /* Hover effect */
+  div[data-baseweb="select"] > div:hover,
+  input[type=number]:hover {
+  background-color: #76787A;
+  }
+  span.st-bj.st-cf.st-ce.st-f3.st-f4.st-af {
+  font-size: 0.6em;
+  }
+  @media (max-width: 1024px) {
+    span.st-bj.st-cf.st-ce.st-f3.st-f4.st-af {
+    font-size: 0.8em;
+    }
+  }
+  
+  /* Media query for small screens */
+  @media (max-width: 1024px) {
+  div[data-baseweb="select"] > div,
+  input[type=number] {
+    font-size: 0.8em;
+    height: 3em;
+  }
+  .stMultiSelect [data-baseweb="select"] > div,
+  .stMultiSelect [data-baseweb="tag"] {
+    height: auto !important;
+  }
+  }
+  button[title="View fullscreen"]{
+    visibility: hidden;
+    }
+  </style>
+""", unsafe_allow_html=True)
+
+line1 = '<hr class="line1" style="height:0.1em; border:0em; background-color: #FCBC24; margin-top: 0em; margin-bottom: -2em;">'
+line_media_query1 = '''
+    <style>
+    @media (max-width: 1024px) {
+        .line1 {
+            padding: 0.3em;
+        }
+    }
+    </style>
+'''
+
+line2 = '<hr class="line2" style="height:0.1em; border:0em; background-color: #FAFAFA; margin-top: 0em; margin-bottom: -2em;">'
+line_media_query2 = '''
+    <style>
+    @media (max-width: 1024px) {
+        .line2 {
+            padding: 0.05em;
+        }
+    }
+    </style>
+'''
+
+header = """
+    <style>
+        :root {{
+            --base-font-size: 1vw;  /* Define your base font size here */
+        }}
+
+        .header {{
+            font-family:sans-serif; 
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            background-image: url('data:image/png;base64,{}');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            filter: brightness(0.9) saturate(0.8);
+            opacity: 1;
+            color: #FAFAFA;
+            text-align: left;
+            padding: 0.4em;  /* Convert 10px to em units */
+            z-index: 1;
+            display: flex;
+            align-items: center;
+        }}
+        .middle-column {{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            float: center;            
+            width: 100%;
+            padding: 2em;  /* Convert 10px to em units */
+        }}
+        .middle-column img {{
+            max-width: 200%;
+            display: inline-block;
+            vertical-align: middle;
+        }}
+        .clear {{
+            clear: both;
+        }}
+        body {{
+            margin-top: 1px;
+            font-size: var(--base-font-size);  /* Set the base font size */
+        }}
+        @media screen and (max-width: 1024px) {{
+        .header {{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 3em;
+       }}
+
+        .middle-column {{
+            width: 100%;  /* Set width to 100% for full width on smaller screens */
+            justify-content: center;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            float: center;
+            margin-bottom: 0em;  /* Adjust margin for smaller screens */
+            padding: 0em;
+        }}
+        .middle-column img {{
+            width: 30%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            float: center;
+          }}
+    }}
+    </style>
+    <div class="header">
+        <div class="middle-column">
+            <img src="data:image/png;base64,{}" class="img-fluid" alt="comrate_logo" width="8%">
+        </div>
+    </div>
+"""
+
+# Replace `image_file_path` with the actual path to your image file
+image_file_path = "images/oxbrain_header_background.jpg"
+with open(image_file_path, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode()
+
+st.markdown(header.format(encoded_string, img_to_bytes("images/oxbrain_logo_trans.png")),
+            unsafe_allow_html=True)
 
 
 
