046f8adb939c80612c29633c5a1e1071eefb9409
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 13:37:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1075,7 +1075,7 @@ def generate_images(image_description, n_variations):
         n=n_variations,
         size="256x256")
     except openai.error.InvalidRequestError:
-        st.write("True")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
