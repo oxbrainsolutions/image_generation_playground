d3834a81c1e1c1857efee759f75eb6f5c552d626
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 12:41:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1635,7 +1635,7 @@ if st.session_state.submit_confirm1 == True:
     st.write("")
     st.write("")
     spinner = st.markdown(marker_spinner_css, unsafe_allow_html=True)
-    spinner_image = st.markdown(spinner_image_css.format(img_to_bytes("images/spinner_image.png")), unsafe_allow_html=True)
+    spinner_image = st.markdown(spinner_image_css.format(img_to_bytes("images/oxbrain_spinner.png")), unsafe_allow_html=True)
     generated_images = generate_images(st.session_state.user_image_description, st.session_state.user_n_variations)
     display_images(generated_images)
     spinner.empty()
