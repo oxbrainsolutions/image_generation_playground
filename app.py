2a29d1b7efe91a16bd0f7b13c1282b323cef8946
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 14:06:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1077,6 +1077,7 @@ def generate_images(image_description, n_variations):
     except openai.error.Timeout as e:
         #Handle timeout error, e.g. retry or log
         print(f"OpenAI API request timed out: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1095,6 +1096,7 @@ def generate_images(image_description, n_variations):
     except openai.error.APIError as e:
         #Handle API error, e.g. retry or log
         print(f"OpenAI API returned an API Error: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1113,6 +1115,7 @@ def generate_images(image_description, n_variations):
     except openai.error.APIConnectionError as e:
         #Handle connection error, e.g. check network or log
         print(f"OpenAI API request failed to connect: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1131,6 +1134,7 @@ def generate_images(image_description, n_variations):
     except openai.error.InvalidRequestError as e:
         #Handle invalid request error, e.g. validate parameters or log
         print(f"OpenAI API request was invalid: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1149,6 +1153,7 @@ def generate_images(image_description, n_variations):
     except openai.error.AuthenticationError as e:
         #Handle authentication error, e.g. check credentials or log
         print(f"OpenAI API request was not authorized: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1167,6 +1172,7 @@ def generate_images(image_description, n_variations):
     except openai.error.PermissionError as e:
         #Handle permission error, e.g. check scope or log
         print(f"OpenAI API request was not permitted: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
@@ -1185,6 +1191,7 @@ def generate_images(image_description, n_variations):
     except openai.error.RateLimitError as e:
         #Handle rate limit error, e.g. wait or log
         print(f"OpenAI API request exceeded rate limit: {e}")
+        st.error("Error")
         st.session_state.modal2.open()
         if st.session_state.modal2.is_open():
             with st.session_state.modal2.container():
