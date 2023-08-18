674bd3bfb59557e8932e1daefd3f6aec30dde50e
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 10:32:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def generate_images(image_description, n_variations):
   img_response = openai.Image.create(
     prompt = image_description,
     n=n_variations,
-    size="512x512")
+    size="256x256")
   
   for idx, data in enumerate(img_response['data']):
       img_url = data['url']
