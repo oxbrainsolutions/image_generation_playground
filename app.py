3210611eda33f9b6c75254e45c043babe2e79e06
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 17:39:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -378,10 +378,6 @@ def generate_images2(image_description, n_variations):
     
     return images
 
-class StopException(Exception):
-    pass
-
-
 def generate_images(image_description, n_variations):
 
     images = []
@@ -395,37 +391,44 @@ def generate_images(image_description, n_variations):
         #Handle timeout error, e.g. retry or log
         print(f"OpenAI API request timed out: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.APIError as e:
         #Handle API error, e.g. retry or log
         print(f"OpenAI API returned an API Error: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.APIConnectionError as e:
         #Handle connection error, e.g. check network or log
         print(f"OpenAI API request failed to connect: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.InvalidRequestError as e:
         #Handle invalid request error, e.g. validate parameters or log
         print(f"OpenAI API request was invalid: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.AuthenticationError as e:
         #Handle authentication error, e.g. check credentials or log
         print(f"OpenAI API request was not authorized: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.PermissionError as e:
         #Handle permission error, e.g. check scope or log
         print(f"OpenAI API request was not permitted: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
     except openai.error.RateLimitError as e:
         #Handle rate limit error, e.g. wait or log
         print(f"OpenAI API request exceeded rate limit: {e}")
         st.session_state.error_indicator = True
-        raise StopException()
+        st.error("Your request was rejected by the safety system.")
+        pass
 
     if st.session_state.error_indicator == False:
         for idx, data in enumerate(img_response['data']):
