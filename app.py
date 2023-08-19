535a51e0c6b9c54f7526c10ec382fca558f1df88
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-18 17:33:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -393,37 +393,37 @@ def generate_images(image_description, n_variations):
         #Handle timeout error, e.g. retry or log
         print(f"OpenAI API request timed out: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.APIError as e:
         #Handle API error, e.g. retry or log
         print(f"OpenAI API returned an API Error: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.APIConnectionError as e:
         #Handle connection error, e.g. check network or log
         print(f"OpenAI API request failed to connect: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.InvalidRequestError as e:
         #Handle invalid request error, e.g. validate parameters or log
         print(f"OpenAI API request was invalid: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.AuthenticationError as e:
         #Handle authentication error, e.g. check credentials or log
         print(f"OpenAI API request was not authorized: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.PermissionError as e:
         #Handle permission error, e.g. check scope or log
         print(f"OpenAI API request was not permitted: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
     except openai.error.RateLimitError as e:
         #Handle rate limit error, e.g. wait or log
         print(f"OpenAI API request exceeded rate limit: {e}")
         st.session_state.error_indicator = True
-        return images
+        raise StopException()
 
     if st.session_state.error_indicator == False:
         for idx, data in enumerate(img_response['data']):
@@ -434,6 +434,9 @@ def generate_images(image_description, n_variations):
             images.append(img)
     
     return images
+
+class StopException(Exception):
+    pass
 
 def display_images(images):
     num_images = len(images)
