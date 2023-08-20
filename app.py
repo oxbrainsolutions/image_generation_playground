fb590df18fca6a65ea3296c88f4055b984e9452c
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-20 08:10:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -975,8 +975,6 @@ if submit_button1:
           del st.session_state.user_generated_images
 
 if st.session_state.submit_confirm1 == True:
-    create_prompt_text_field.empty()
-    create_prompt_button_field.empty()
 
     if st.session_state.error_indicator == False:
         spinner = st.markdown(marker_spinner_css, unsafe_allow_html=True)
@@ -986,6 +984,8 @@ if st.session_state.submit_confirm1 == True:
     if st.session_state.error_indicator == False:
         display_images(st.session_state.user_generated_images)
         spinner.empty()
+        create_prompt_text_field.empty()
+        create_prompt_button_field.empty()
         reset_button_field.button("Reset", key="key5", on_click=reset1)
     else:
         spinner.empty()
