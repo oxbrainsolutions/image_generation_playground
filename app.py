de73b5070273b19e32054db2158f6685e3e9d92f
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-23 10:01:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -818,9 +818,9 @@ st.markdown(header.format(encoded_string, img_to_bytes("images/oxbrain_logo_tran
 spinner = st.empty()
 
 with st.sidebar:
-    header = '<p class="text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.6em; ">DISCLAIMER: The image synthesis and generation capabilities provided in the playground are facilitated through advanced machine learning technology developed by OpenAI. This playground may exhibit inherent limitations associated with the accuracy of generated images and is intended for educational purposes only.</p></span></p>'
+    sidebar_header_text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.6em; ">DISCLAIMER: The image synthesis and generation capabilities provided in the playground are facilitated through advanced machine learning technology developed by OpenAI. This playground may exhibit inherent limitations associated with the accuracy of generated images and is intended for educational purposes only.</p></span></p>'
 
-    st.sidebar.markdown(header, unsafe_allow_html=True)
+    st.markdown(sidebar_header_text, unsafe_allow_html=True)
     subheader_text1 = '''<p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Generate an Image</span></p>'''
     st.markdown(subheader_media_query + subheader_text1, unsafe_allow_html=True)
     st.markdown(line_media_query1 + line1, unsafe_allow_html=True)
